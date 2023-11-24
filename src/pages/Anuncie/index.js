b5dcb30a05a708e71e6283186ab045ea7f576079
Ryan Cavalcanti <ryancavalcanti192@gmail.com>
2023-11-23 23:25:19 -0300

    criando o formulário

--- src/pages/Anuncie/index.js
+++ src/pages/Anuncie/index.js
@@ -1,8 +1,30 @@
+import Header from "components/Header";
+import { useSelector } from "react-redux";
 
 export default function Anuncie() {
+    const categorias = useSelector(state => state.categorias.map(({ nome, id }) => ({ nome, id})));
+
     return(
         <div>
-            Anuncie
+            <Header
+                titulo='Anuncie aqui!'
+                descricao='Anuncie seu produto no melhor site do Brasil!'
+            />
+            <form>
+                <input placeholder="Nome do produto" alt="Nome do produto"/>
+                <input placeholder="Descrição do produto" alt="Descrição do produto"/>
+                <input placeholder="URL da imagem do produto" alt="URL da imagem do produto"/>
+                <select>
+                    <option value='' disabled >Selecione a categoria</option>
+                    {categorias.map(categoria => (
+                        <option key={categoria.id} value={categoria.id}>
+                            {categoria.nome}
+                        </option>
+                    ))}
+                </select>
+                <input type="number" placeholder="Preço do produto"/>
+                <button type="submit"> Cadastrar produto </button>
+            </form>
         </div>
     )
 }
